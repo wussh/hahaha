0ae1ccbc69b519a251dcc3d585c5ef4e71601008
Wush <andrianta.321@gmail.com>
2022-08-17 02:09:45 +0700

    2

--- 387. First Unique Character in a String/main.go
+++ 387. First Unique Character in a String/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 // Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 // Example 1:
@@ -16,7 +19,7 @@ import "fmt"
 // Input: s = "aabb"
 // Output: -1
 
-func firstUniqChar(s string) int {
+func firstUniqChar1(s string) int {
 	d := map[byte]int{}
 
 	// Count each character.
@@ -34,7 +37,47 @@ func firstUniqChar(s string) int {
 	// If there's no unique character then return -1.
 	return -1
 }
+func firstUniqChar2(s string) int {
+	// This is a constant space allocation (ie: always 26)
+	var counts = make([]int, 26)
+
+	// Insert all the character's count into counts array
+	for i := 0; i < len(s); i++ {
+		counts[s[i]-'a']++
+	}
+
+	// Find the first element with count 1
+	for i := 0; i < len(s); i++ {
+		if counts[s[i]-'a'] == 1 {
+			// The reason for - 'A', is that it "shifts" the ascii/unicode value so that A - Z have values 0 - 25.
+			// And are thus more suitable as an array index.
+			//'A' - 'A' == 0
+			// 'B' - 'A' == 1
+			// 'C' - 'A' == 2
+			return i
+		}
+	}
+
+	return -1
+}
+
+func TestFirstUniqChar(t *testing.T) {
+	cases := []struct {
+		in   string
+		want int
+	}{
+		{"leetcode", 0},
+		{"loveleetcode", 2},
+		{"aabb", -1},
+	}
+	for _, c := range cases {
+		got := firstUniqChar1(c.in)
+		if got != c.want {
+			t.Errorf("FirstUniqChar(%s) == %d, want %d", c.in, got, c.want)
+		}
+	}
+}
 
 func main() {
-	fmt.Println(firstUniqChar("leetcode"))
+	fmt.Println(firstUniqChar1("leetcode"))
 }
